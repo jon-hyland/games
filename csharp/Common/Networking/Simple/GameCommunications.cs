76b70efea12aebe87474ffaea18064d3be40ba18
John Hyland <jonhyland@hotmail.com>
2020-04-24 19:16:38 -0700

    ..

--- csharp/Common/Networking/Simple/GameCommunications.cs
+++ csharp/Common/Networking/Simple/GameCommunications.cs
@@ -1,6 +1,7 @@
 ﻿using Common.Error;
 using Common.Networking.Simple.Discovery;
 using Common.Networking.Simple.Packets;
+using Common.Threading;
 using SimpleTCP;
 using System;
 using System.Collections.Generic;
@@ -9,6 +10,13 @@ using System.Threading;
 
 namespace Common.Networking.Simple
 {
+    /// <summary>
+    /// Simple function SetPlayer.. disconnects if connected.
+    /// Simple function SendCommand.. reconnects if not connected (no matter if accepted).
+    /// Move connect logic outside class?  Simplify logic?
+    /// Add event for connection request, function for user connection response.
+    /// Both places set a connection flag.  Change from ConnectionState?
+    /// </summary>
     public class GameCommunications : IDisposable
     {
         //const
@@ -27,9 +35,11 @@ namespace Common.Networking.Simple
         private readonly SimpleTcpClient _dataClient;
         private readonly SimpleTcpServer _dataServer;
         private readonly CommandManager _commandManager;
+        private readonly SimpleTimer _timer;
+        private ConnectionState _connectionState;
         private ushort _commandSequence;
+        private IPAddress _remoteIP;
         private Player _remotePlayer;
-        private IPAddress _remoteIP => ((IPEndPoint)_dataClient?.TcpClient?.Client?.RemoteEndPoint)?.Address ?? IPAddress.None;
 
         //public
         public string GameTitle => _gameTitle;
@@ -56,12 +66,15 @@ namespace Common.Networking.Simple
             _dataClient = new SimpleTcpClient();
             _dataServer = new SimpleTcpServer();
             _commandManager = new CommandManager();
+            _timer = new SimpleTimer(Timer_Callback, 15, false);
+            _connectionState = ConnectionState.NotConnected;
             _commandSequence = 0;
+            _remoteIP = null;
             _remotePlayer = null;
 
             //events
             _discoveryServer.PlayerAnnounced += (p) => _discoveredPlayers.AddOrUpdatePlayer(p);
-            _dataServer.DataReceived += (s, m) => DataReceived(m);
+            _dataServer.DataReceived += (s, m) => DataReceived(m?.Data);
         }
 
         /// <summary>
@@ -74,6 +87,7 @@ namespace Common.Networking.Simple
             _discoveryClient.Dispose();
             _dataClient.Dispose();
             _commandManager.Dispose();
+            _timer.Dispose();
         }
 
         /// <summary>
@@ -89,6 +103,9 @@ namespace Common.Networking.Simple
 
             //start data server
             _dataServer.Start(_gamePort);
+
+            //start maintenance timer
+            _timer.Start();
         }
 
         /// <summary>
@@ -96,6 +113,7 @@ namespace Common.Networking.Simple
         /// </summary>
         public void Stop()
         {
+            _timer.Stop();
             _dataClient.Disconnect();
             _dataServer.Stop();
             _discoveryClient.Stop();
@@ -135,14 +153,17 @@ namespace Common.Networking.Simple
             CommandResult result = CommandResult.Unspecified;
             try
             {
-                //end any existing connection
+                //close any existing connection
                 _remotePlayer = null;
                 _dataClient.Disconnect();
+                _connectionState = ConnectionState.NotConnected;
 
-                //reconnect
+                //connect socket
                 _dataClient.Connect(player.IP.ToString(), _gamePort);
+                _remoteIP = player.IP;
 
                 //send connect-request command
+                _connectionState = ConnectionState.PendingAcceptance_SendSide;
                 result = SendCommand(1, TimeSpan.FromMilliseconds(CONNECT_REPLY_TIMEOUT_MS));
 
                 //do stuff..
@@ -150,20 +171,29 @@ namespace Common.Networking.Simple
             catch (Exception ex)
             {
                 _errorHandler?.LogError(ex);
+                _connectionState = ConnectionState.Error;
             }
             return result;
         }
 
         /// <summary>
-        /// 
+        /// Sends command, blocks until response or timeout.
         /// </summary>
         public CommandResult SendCommand(ushort type, TimeSpan timeout)
         {
             try
             {
                 //not connected?
-                if (_dataClient?.TcpClient?.Client?.Connected != true)
-                    throw new Exception("Data client not connected");
+                if (type == 1)
+                {
+                    if ((_connectionState != ConnectionState.Connected) && (_connectionState != ConnectionState.PendingAcceptance_SendSide))
+                        throw new Exception("Data client not connected");
+                }
+                else
+                {
+                    if (_connectionState != ConnectionState.Connected)
+                        throw new Exception("Data client not connected");
+                }
 
                 //vars
                 ushort sequence;
@@ -217,20 +247,75 @@ namespace Common.Networking.Simple
             catch (Exception ex)
             {
                 _errorHandler?.LogError(ex);
+                _connectionState = ConnectionState.Error;
             }
             return CommandResult.Error;
         }
 
-        private void DataReceived(Message message)
+        
+        private void DataReceived(byte[] bytes)
         {
             try
             {
-                if (message?.Data == null)
+                if (bytes == null)
                     return;
                 
-                PacketBase packet = PacketBase.FromBytes(message.Data);
+                PacketBase packet = PacketBase.FromBytes(bytes);
                 if (packet == null)
                     return;
+
+                if ((packet.GameTitle != _gameTitle) || (packet.GameVersion != _gameVersion))
+                    return;
+
+                if (packet.Type == PacketType.CommandRequest)
+                {
+                    CommandRequestPacket p = (CommandRequestPacket)packet;
+                    if (p.CommandType == 1)
+                    {
+
+                    }
+
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                _errorHandler?.LogError(ex);
+                _connectionState = ConnectionState.Error;
+            }
+        }
+
+        /// <summary>
+        /// Fired by timer, performs maintenance.
+        /// </summary>
+        private void Timer_Callback()
+        {
+            try
+            {
+                //determine if error state
+                if (_connectionState == ConnectionState.Connected)
+                    if (_dataClient?.TcpClient?.Connected != true)
+                        _connectionState = ConnectionState.Error;
+
+                //if error, close and reconnect
+                if (_connectionState == ConnectionState.Error)
+                {
+                    _remotePlayer = null;
+                    _dataClient.Disconnect();
+                    if (_remoteIP != null)
+                        _dataClient.Connect(_remoteIP.ToString(), _gamePort);
+                    else
+                        _connectionState = ConnectionState.NotConnected;
+                    return;
+                }
+
+                //receive-side accepted
+                if (_connectionState == ConnectionState.PendingAcceptance_ReceiveSide)
+                {
+                    //connect to send-side server
+
+                }
 
 
 
@@ -241,5 +326,17 @@ namespace Common.Networking.Simple
             }
         }
 
+    }
+
+    /// <summary>
+    /// Represents the system's connection states.
+    /// </summary>
+    public enum ConnectionState
+    {
+        NotConnected,
+        Error,
+        PendingAcceptance_SendSide,
+        PendingAcceptance_ReceiveSide,
+        Connected
     }
 }
