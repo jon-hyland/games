b87c95ee047d95f44400210eddee02391a4d1d27
John Hyland <jonhyland@hotmail.com>
2020-05-05 18:41:48 -0700

    ..

---
+++ csharp/Bricker/Game/GameState.cs
@@ -0,0 +1,14 @@
+﻿namespace Bricker.Game
+{
+    /// <summary>
+    /// Represents the run state of the game.
+    /// </summary>
+    public enum GameState
+    {
+        NotPlaying,
+        GameLive,
+        GamePaused,
+        GameOver,
+        GameOver_OpponentLoss
+    }
+}
