91b2608ea1736cd1ce3a8d330228594ff1b09b8e
John Hyland <jonhyland@hotmail.com>
2020-05-19 22:40:05 -0700

    ..

--- csharp/Bricker/Rendering/Properties/SettingsProperties.cs
+++ csharp/Bricker/Rendering/Properties/SettingsProperties.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace Bricker.Rendering.Properties
 {
-    class SettingsProperties
+    public class SettingsProperties
     {
+        public SettingsMenuItem[] Items { get; }
+        public 
+
+    }
+
+    public class SettingsMenuItem
+    {
+        public string OnCaption { get; }
+        public string OffCaption { get; }
+        public bool Value { get; set; }
+        public string Caption => Value ? OnCaption : OffCaption;
+
+        public SettingsMenuItem(string onCaption, string offCaption, bool value)
+        {
+            OnCaption = onCaption;
+            OffCaption = offCaption;
+            Value = value;
+        }
     }
 }
