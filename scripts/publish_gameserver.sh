e9acdca9e7aef5aca5337db41058d05e20ad6097
John Hyland <jonhyland@hotmail.com>
2020-05-23 12:16:46 -0700

    ..

--- scripts/publish_gameserver.sh
+++ scripts/publish_gameserver.sh
@@ -1,47 +1,42 @@
 #!/bin/bash
 
+# ensure run as root
 if [ "$EUID" -ne 0 ]
 then
     echo "Must be run as root"
     exit
 fi
 
+# break on error
 set -e
 
-
-if [ $(dpkg-query -W -f='${Status}' git 2>/dev/null | grep -c "ok installed") -eq 0 ]
-then
-    echo "Installing Git.."
-    apt-get install git
-fi
-
-sudo -u pi git config --global credential.helper "cache --timeout=3600"
-sudo -u pi git config --global user.name "John Hyland"
-sudo -u pi git config --global user.email "jonhyland@hotmail.com"
-sudo -u pi mkdir -p /home/pi/git/
-
+# clone (or pull) 'games' repo
 if [ ! -d "/home/pi/git/games" ]
 then
-    echo "Cloning repository.."
+    echo "Cloning 'games' repository.."
     sudo -u pi git clone "https://github.com/jon-hyland/games.git" "/home/pi/git/games/"
 else
-    echo "Pulling repository.."
+    echo "Pulling 'games' repository.."
     sudo -u pi git -C "/home/pi/git/games" pull
 fi
 
+# stop 'gameserver' service
 if systemctl list-units | grep -Fq 'gameserver.service'
 then
     echo "Stopping Game Server.."
     systemctl stop gameserver.service
 fi
 
-echo "Building Game Server.."
+# build and publish 'gameserver' project
+echo "Building and publishing Game Server.."
 dotnet publish --output /usr/share/gameserver/ /home/pi/git/games/csharp/GameServer/GameServer.csproj
 
+# create symbolic links
 echo "Creating symbolic links.."
 sudo -u pi ln -sf /usr/share/gameserver /home/pi/gameserver
 sudo -u pi ln -sf /usr/share/gameserver/LogFile.txt /home/pi/log_file.txt
 
+# create service file
 echo "Creating service file.."
 rm -f /etc/systemd/system/gameserver.service
 cat <<EOF >/etc/systemd/system/gameserver.service
@@ -58,10 +53,13 @@ Restart=on-abort
 WantedBy=multi-user.target
 EOF
 
+# refresh services
 echo "Refreshing services.."
 systemctl daemon-reload
 
+# start 'gameserver' service
 echo "Starting Game Server.."
 systemctl start gameserver.service
 
-echo "Publish complete"
+# success
+echo "Operation success"
--- scripts/restart_gameserver.sh
+++ scripts/restart_gameserver.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+# ensure run as root
 if [ "$EUID" -ne 0 ]
-  then echo "Must be run as root"
-  exit
+then
+    echo "Must be run as root"
+    exit
 fi
 
+# break on error
 set -e
 
+# restart 'gameserver' service
+echo "Restarting Game Server.."
 systemctl restart gameserver.service
